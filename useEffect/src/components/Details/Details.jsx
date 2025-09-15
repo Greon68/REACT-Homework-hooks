67f47a07b0b290532d38310b7d310c2909dfb0b6
Greon68 <greon-ya@yandex.ru>
2025-09-15 14:13:10 +0300

    commit-2

--- useEffect/src/components/Details/Details.jsx
+++ useEffect/src/components/Details/Details.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 import { loadData } from '../../api/loadData';
 import S from './Details.module.css'
 
-export const Details = ({id,name}) => {
 
+
+export const Details = ({id}) => {
     console.log('id-',id);
-    console.log('name-',name);
+    
+    const qyeryParam = `?key=${Math.random()}`
 
-    const [data , setData]= useState({});
+    const [data , setData]= useState(null);
 
     useEffect(()=>{
         loadData(`${id}.json`)
@@ -15,25 +17,16 @@ export const Details = ({id,name}) => {
     },[id] )
 
     console.log("Данные о пользователе data -",data)
-    console.log("data.name - ", data.name)
-    console.log("data.id - ", data.id)
-    console.log("data.details - ", data.details)
-    // console.log("data.details.city - ", data.details.city)
-
- 
+  
     return ( 
             data && (
                 <div className={S.user}>
-                    <img src={data.avatar} className={S.image} />
-    
+                    <img src={data.avatar + qyeryParam} className={S.image} />
                     <h2> Name : {data.name}</h2>
-                     <h2> Id: {data.id}</h2>
-                     <h2> avatar: {data.avatar}</h2>
+                    <h2> Company : <em className={S.details}>{data.details.company}</em> </h2>      
+                    <h2> City: <em className={S.details}>{data.details.city}</em> </h2>
+                    <h2> Position : <em className={S.details}>{ data.details.position }</em> </h2>
 
-                     {/* *** Код ниже не срабатывает *** */}
-                    {/* <h2> Company : {data.details.company}</h2> */}     
-                    {/* <h2> City: {data.details.city}</h2> */}
-                    
                 </div>
             )
         )
